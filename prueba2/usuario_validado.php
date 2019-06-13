86ea0f495f6f4d9cdcbb27179bccff3843937502
David Marco <davidmarconinamendoza@gmail.com>
2019-06-13 17:15:28 -0400

    primer commit en git hub

---
+++ prueba2/usuario_validado.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+</head>
+<body>
+    <script type="text/javascript">
+        alert("Se registro al Usuario: <?php echo $usu ?> con el password: <?php echo $pass?>");
+        location.href="<?=base_url()?>usuario/index";
+    </script>
+
+</body>
+</html>
